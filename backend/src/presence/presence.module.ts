3734f4b342fc9ca84242e64f57c787edcd947fac
DutheeshChelaka <dutheeshwork@gmail.com>
2026-03-09 22:54:47 +0530

    Update presence module

--- backend/src/presence/presence.module.ts
+++ backend/src/presence/presence.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PresenceGateway } from './presence.gateway';
 import { RedisService } from '../common/redis.service';
+import { PrismaService } from '../common/prisma.service';
 
 @Module({
-  providers: [PresenceGateway, RedisService],
+  providers: [PresenceGateway, RedisService, PrismaService],
   exports: [PresenceGateway],
 })
 export class PresenceModule {}
